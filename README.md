adcb0a4edfd206e0f450a3319fcfff94410022a9
Paul Ambrose <pambrose@mac.com>
2019-02-20 22:08:44 -0800

    Misc cleanup

--- README.md
+++ README.md
@@ -1,3 +1,4 @@
 # Declarative Pipelines in Kotlin
 
 The repo goes along with [this post](https://medium.com/@desaismital/declarative-pipelines-in-kotlin-b9e18e77f2c5).
+
--- src/main/kotlin/org/athenian/NumbersGenerator.kt
+++ src/main/kotlin/org/athenian/NumbersGenerator.kt
@@ -9,16 +9,6 @@ fun evenNumbers(max: Int): Sequence<Int> {
     }
 }
 
-fun oddNumbers(max: Int) =
-    sequence {
-        IntRange(1, max)
-            .forEach {
-                if (it % 2 != 0)
-                    yield(it)
-            }
-    }
-
-
 fun main() {
     print("Even numbers <= 10: ")
     for (i in evenNumbers(10))
@@ -29,11 +19,10 @@ fun main() {
     evenNumbers(10).forEach { print("$it ") }
     println()
 
-    println("Even numbers <= 10: (${evenNumbers(10).joinToString(", ")})")
+    println("Even numbers <= 10: ${IntRange(0, 10).filter { it % 2 == 0 }}")
 
     println("Even numbers <= 10: ${evenNumbers(10).toList()}")
-    println("Odd numbers <= 10: ${oddNumbers(10).toList()}")
-
+    println("Even numbers <= 10: (${evenNumbers(10).joinToString(", ")})")
 
     println("Min even number <= 10: ${evenNumbers(10).min()}")
     println("Max even number <= 10: ${evenNumbers(10).max()}")
@@ -41,9 +30,37 @@ fun main() {
     println("Average of even numbers <= 10: ${evenNumbers(10).average()}")
     println("First even number <= 10: ${evenNumbers(10).first()}")
 
+    // Chained
+    val v1 =
+        evenNumbers(10)
+            .filter { it <= 8 }
+            .average()
+    println("Average of even numbers <= 8: $v1")
+
+    val v2 =
+        evenNumbers(10)
+            .filter { it <= 8 }
+            .map { it * it }
+            .average()
+    println("Average of even numbers <= 8 squared : $v2")
+
+    val oddNumbers =
+        sequence {
+            for (i in 0..10) {
+                if (i % 2 != 0)
+                    yield(i)
+            }
+        }
+
+    val allNums =
+        evenNumbers(10)
+            .plus(oddNumbers)
+            .sorted()
+            .toList()
+
+    println("All numbers <= 10 $allNums")
+
     // Boolean operations
     println("Any even numbers <= 10 <= 6: ${evenNumbers(10).any { it <= 6 }}")
     println("All even numbers <= 10 <= 6: ${evenNumbers(10).all { it <= 6 }}")
-
-    println("All numbers <= 10 ${evenNumbers(10).plus(oddNumbers(10)).sorted().toList()}")
 }
