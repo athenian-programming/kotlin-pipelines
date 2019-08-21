03b2d49afeb1a8ebbec127c288ff7103e740c567
Paul Ambrose <pambrose@mac.com>
2019-08-21 10:14:08 -0700

    Update jars

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,6 +3,7 @@ import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
     kotlin("jvm") version "1.3.41"
+    id("com.github.ben-manes.versions") version "0.22.0"
 }
 
 group = "org.athenian"
